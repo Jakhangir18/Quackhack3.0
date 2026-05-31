070bdea4a92ac56fc32c4a112a3f75056434f067
Marcus T. <tinw@oregonstate.edu>
2026-05-31 02:04:58 -0700

    added python scripts for individual dots

---
+++ tests/dot6.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, __file__.rsplit("/tests", 1)[0])
+from output.motors import buzz_dot, motors
+
+try:
+    buzz_dot(6)
+finally:
+    for m in motors.values():
+        m.off()
+        m.close()
